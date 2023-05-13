735816667347f59d24c6749f61c90a7777e34595
tomatoaiu <12771142+tomatoaiu@users.noreply.github.com>
2023-05-13 21:43:44 +0900

    feat: コンテンツ描画後に色を変更する

--- src/background.ts
+++
@@ -1,7 +0,0 @@
-chrome.action.onClicked.addListener(async () => {
-  const tabs = await chrome.tabs.query({ active: true, currentWindow: true });
-  await chrome.tabs.sendMessage(tabs[0]?.id || 0, {
-    message: 'dummy',
-  });
-  return true;
-});
--- src/content.ts
+++ src/content.ts
@@ -1,5 +1,39 @@
-chrome.runtime.onMessage.addListener((_message, _sender, sendResponse) => {
-  alert(`Hello Chrome Extension!\nThis page title : ${document.title}`);
-  sendResponse();
-  return true;
-});
+const getHeader = (): HTMLElement | null => {
+  const header: HTMLElement | null = document.querySelector(
+    '#repository-container-header',
+  );
+  return header;
+};
+
+const getStatusLabel = (
+  header: HTMLElement,
+): HTMLElement | null | null => {
+  const statusLabel: HTMLCollectionOf<Element> | null = header
+    .getElementsByClassName(
+      'Label Label--secondary v-align-middle mr-1',
+    );
+
+  return statusLabel?.[0] as HTMLElement | null;
+};
+
+const doPaint = () => {
+  const header = getHeader();
+  if (header) {
+    const toPublicColor = () => header.style.backgroundColor = '#2a2';
+    const toPrivateColor = () => header.style.backgroundColor = '#a22';
+
+    const label = getStatusLabel(header);
+
+    if (label && label.innerText === 'Public') {
+      toPublicColor();
+    } else if (label && label.innerText === 'Private') {
+      toPrivateColor();
+    } else if (header.innerText.match(/Public/)) {
+      toPublicColor();
+    } else if (header.innerText.match(/Private/)) {
+      toPrivateColor();
+    }
+  }
+};
+
+doPaint();
